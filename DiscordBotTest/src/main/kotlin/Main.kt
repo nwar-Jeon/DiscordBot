2797d205ee6ae7ad2aa71508e8f0deb95926bcfc
Seungmin Jeon <nwar0825@naver.com>
2021-02-07 16:34:13 +0900

    Update Main.kt

--- DiscordBotTest/src/main/kotlin/Main.kt
+++ DiscordBotTest/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import net.dv8tion.jda.api.JDABuilder
 
 fun main() {
-    val jda = JDABuilder.createDefault("ODA3MTI1NDI2MDMxNTU4Njk3.YBzcIQ.lfPpru2UJiU95CcUc2byALpAW9Y").build()
+    val jda = JDABuilder.createDefault("").build()
 
     jda.addEventListener(Starter())
 }
